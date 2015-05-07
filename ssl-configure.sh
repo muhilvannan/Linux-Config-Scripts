22eb65e642a74f1187054137053afefadd8f1a49
Muhil Vannan <muhil@accentdesign.co.uk>
2015-05-07 10:17:29 +0100

    ssl directory configuration

---
+++ ssl-configure.sh
@@ -0,0 +1,9 @@
+mkdir /etc/httpd/ssl
+mkdir /etc/httpd/ssl/bundles
+mkdir /etc/httpd/ssl/certs
+mkdir /etc/httpd/ssl/keys
+mkdir /etc/httpd/ssl/csr
+chmod 644 /etc/httpd/ssl/bundles
+chmod 644 /etc/httpd/ssl/certs
+chmod 600 /etc/httpd/ssl/keys
+chmod 644 /etc/httpd/ssl/csr
